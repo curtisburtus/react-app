2eda91713e0bfdc1e04fff62a4d19c9c86ee7ebf
Curtis Hansen <curtishansen3@gmail.com>
2023-06-26 15:59:17 -0700

    UseFunctionWithConditional

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
 function ListGroup() {
   const items = ["New York", "Portland", "Tokyo", "London", "Paris"];
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
 
   items.map((item) => <li>{item}</li>);
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
