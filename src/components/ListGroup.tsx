0bb93623d8f050669b6eab78ad0c5854efebc13e
Curtis Hansen <curtishansen3@gmail.com>
2023-06-26 16:38:58 -0700

    bootstrap fixes

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,7 +7,9 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
