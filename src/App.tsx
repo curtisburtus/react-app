be4bbbde94f11f7ecab262dde01713e0f0bb71ba
Curtis Hansen <curtishansen3@gmail.com>
2023-07-07 17:55:01 -0700

    open and close alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import { Button } from "./components/Button";
 
 function App() {
+  useState(false);
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>Click Me</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Warning</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Click Me</Button>
     </div>
   );
 }
